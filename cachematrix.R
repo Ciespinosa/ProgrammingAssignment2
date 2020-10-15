88e387fac43e44d0f9ab0a106c06ddc850d39e05
Ciespinosa <ciespinosa@utpl.edu.ec>
2020-10-15 18:47:13 -0500

    Create and stored in cache
    The first version of makechcheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions create, store in the cache and recover the cache data
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object 
+## that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+                    mInv <- NULL
+                    get <- function() x
+                    
+                    setInv <- function(solve) mInv <<- solve
+                    getInv <- function() mInv
+                         list(get = get,
+                         setInv = setInv,
+                         getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special
+## "matrix" returned by `makeCacheMatrix` above. If the inverse has
+## already been calculated (and the matrix has not changed), then
+## `cacheSolve` should retrieve the inverse from the cacheWrite 
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+                mInv <- x$getInv()
+                 if(!is.null(mInv)) {
+                message("getting cached data")
+                 return(mInv)
+                        }
+                data <- x$get()
+                mInv <- solve(data, ...)
+                x$setInv(mInv)
+                mInv
 }
